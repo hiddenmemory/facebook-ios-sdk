f739eae5e5556387bddaf390d4ba1fee75ec6dba
Chris Ross <chris@hiddenmemory.co.uk>
2012-06-01 23:54:09 +0100

    Start of the API
    Signed-off-by: Chris Ross <chris@hiddenmemory.co.uk>

---
+++ src/Facebook+Graph.h
@@ -0,0 +1,13 @@
+//
+//  Facebook+Graph.h
+//  Hackbook
+//
+//  Created by Chris Ross on 01/06/2012.
+//  Copyright (c) 2012 hiddenMemory Ltd. All rights reserved.
+//
+
+#import "Facebook.h"
+
+@interface Facebook (Graph)
+
+@end
---
+++ src/Facebook+Graph.m
@@ -0,0 +1,13 @@
+//
+//  Facebook+Graph.m
+//  Hackbook
+//
+//  Created by Chris Ross on 01/06/2012.
+//  Copyright (c) 2012 hiddenMemory Ltd. All rights reserved.
+//
+
+#import "Facebook+Graph.h"
+
+@implementation Facebook (Graph)
+
+@end
